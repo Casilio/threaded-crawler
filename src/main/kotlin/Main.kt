07c39769695312ed659affbac4372ee8c0284158
Pavel Savchuk <pavel.savchuk@influitive.com>
2022-05-30 17:52:30 +0300

    starting something here

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,16 @@
 import kotlinx.coroutines.*
 import java.net.MalformedURLException
 import java.net.URL
+import kotlin.system.exitProcess
 
 fun extractUrls(body: String) {
     Regex("<a.*href=\"(.+?\")")
         .findAll(body)
         .map { it.groups[1]?.value.toString() }
-        .filter {
-            try {
-                URL(it)
-                true
-            } catch (e: MalformedURLException) {
-                false
-            }
-        }.toList()
+        .filter(::isUrl)
+        .toList()
 }
-suspend fun dosome() = coroutineScope {
+suspend fun dosome(flags: Int) = coroutineScope {
     var dispatcher = Dispatchers.IO.limitedParallelism(30)
     val result = (1..30).map {
         async(dispatcher) {
@@ -30,7 +25,37 @@ suspend fun dosome() = coroutineScope {
     println(result)
 }
 
+fun isUrl(url: String): Boolean {
+    return try {
+        URL(url)
+        true
+    } catch (e: MalformedURLException) {
+        false
+    }
+}
+
+fun printHelp() {
+    println("Usage: crawler url term [term]...")
+}
+
 fun main(args: Array<String>) = runBlocking {
+    if (args.size < 2) {
+        printHelp();
+        exitProcess(1);
+    }
+
+    val url = args[0]
+    println(url)
+    println(args.toList())
+
+    if (!isUrl(url)) {
+        System.err.println("Expect first argument to be a url.")
+        exitProcess(2);
+    }
+
+    val terms = args.slice(1..-1)
+    println(terms.toList())
+
 //    val start = System.currentTimeMillis()
 //    dosome()
 //    val finish = System.currentTimeMillis()
